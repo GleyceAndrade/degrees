c751155fd5f16daaa98b8682351acea9b2f41066
GleyceAndrade.git <gleyceandradek@gmail.com>
2025-12-14 18:27:48 -0300

    degrees.py

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -9,7 +9,7 @@ names = {}
 # Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
 people = {}
 
-# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+## Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
 movies = {}
 
 
